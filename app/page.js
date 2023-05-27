92353d85e342c9e4635f3d250e5087fffaa7545c
Prince panwar <princegujjar1.pp@gmail.com>
2023-05-27 13:51:53 +0000

    skill hovers and skills section added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "../component/Hero/Hero"
 import AboutMe from "../component/AboutMe/AboutMe"
+import Skills from "@/component/Skills/Skills"
 
 export default function Home() {
   return (
    <div>
      <Hero/>
      <AboutMe/>
+     <Skills/>
    </div>
   )
 }
---
+++ component/Skills/Skills.jsx
@@ -0,0 +1,45 @@
+import {FaHtml5,FaJava,FaReact,FaNode,FaCss3,FaJs, FaGithub} from "react-icons/fa";
+import{SiFirebase} from "react-icons/si";
+const Skills = () => {
+    return (
+      <div className="skills-container">
+        <h2>Skills</h2>
+        <div className="grid-skills">
+          <div className="skill-card html">
+          <FaHtml5/>
+            <p>HTML</p>
+          </div>
+          <div className="skill-card css">
+            <FaCss3/>
+            <p>CSS</p>
+          </div>
+          <div className="skill-card js">
+            <FaJs/>
+            <p>JavaScript</p>
+          </div>
+          <div className="skill-card react">
+            <FaReact/>
+            <p>React</p>
+          </div>
+          <div className="skill-card node">
+            <FaNode/>
+            <p>Node</p>
+          </div>
+          <div className="skill-card java">
+            <FaJava/>
+            <p>Java</p>
+          </div>
+          <div className="skill-card github">
+            <FaGithub/>
+            <p>GitHub</p>
+          </div>
+          <div className="skill-card Firebase">
+        <SiFirebase/>
+            <p>Firebase</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Skills;
