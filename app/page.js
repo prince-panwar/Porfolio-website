7edf14ef7db022d2072bc0f5f5f641aae021451d
Prince panwar <princegujjar1.pp@gmail.com>
2023-06-08 10:33:37 +0000

    Particle js implememnted

--- app/page.js
+++ app/page.js
@@ -4,10 +4,12 @@ import Skills from "@/component/Skills/Skills"
 import Projects from "@/component/project/project"
 import Navbar from "@/component/Navbar/Navbar"
 import Footer from "@/component/Footer/Footer"
+import PaticleBackground from "@/component/particle/ParticleBackground"
 import "./globals.css"
 export default function Home() {
   return (
    <div>
+    <PaticleBackground/>
     <Navbar/>
      <Hero/>
      <AboutMe/>
---
+++ component/particle/ParticleBackground.js
@@ -0,0 +1,14 @@
+import Particles from "react-tsparticles";
+import ParticleConfig from "./Particleconfig";
+
+
+const PaticleBackground =()=>{
+
+    return(
+        <Particles params={ParticleConfig}>
+            
+        </Particles>
+
+);
+}
+export default PaticleBackground
