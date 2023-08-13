c48055bd38a3b75219cc901ff67a868e1360344f
Prince panwar <princegujjar1.pp@gmail.com>
2023-08-13 12:11:44 +0000

    Project animation

--- component/project/project.js
+++ component/project/project.js
@@ -1,12 +1,13 @@
 import { projectData } from './Project_data.js';
 import { FaGithub,FaFolder } from 'react-icons/fa';
-
+import {Slide} from 'react-awesome-reveal';
 const Projects = () => {
   return (
     <div className="projects-container">
       <h2>Projects</h2>
       <div className="projects-grid">
         {projectData && projectData.map((project) => (
+          <Slide triggerOnce direction='left'>
           <div className="project-card" key={project.id}>
             <div className="project-header">
             <FaFolder className='fa-regular fa-folder-open folder-icon'/>
@@ -17,6 +18,7 @@ const Projects = () => {
             <h3>{project.title}</h3>
             <p>{project.description}</p>
           </div>
+          </Slide>
         ))
         }
       </div>
